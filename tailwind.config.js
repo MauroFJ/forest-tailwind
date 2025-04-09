f53938e6d3b1556e6a2a729c1f0d0da89875b9ff
Mauro FJ <zona.marques@gmail.com>
2025-04-08 22:32:17 -0300

    feat: cores added

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,12 @@ module.exports = {
       colors: {
         'verde-body': '#16281F',
         'verde-escuro': '#0F1C15',
+        'verde-escuro2': '#2E482C',
+        verde: '#17E880',
         'verde-claro': '#91EE77',
+        'verde-claro2': '#ACEF75',
         branco: '#fff',
-        preto: '#000',
+        preto: '#030504',
       },
       fontSize: {
         xs: '12px',
