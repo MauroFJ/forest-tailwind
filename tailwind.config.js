26e48baa0cacdc599a9477608fb11040b0ec20e5
MauroFJ <zona.marques@gmail.com>
2025-08-01 20:20:50 -0300

    fix: github pages fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './index.html',
+    './src/**/*.{,js}'
+  ],
   theme: {
     container: {
       center: true,
